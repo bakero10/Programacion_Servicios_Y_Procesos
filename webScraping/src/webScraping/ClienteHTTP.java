fc89126f2b864558fe08e41c5abf710fa6679cb1
DAM2Alu1 <210162@iessierradeguara.com>
2023-01-27 09:27:06 +0100

    actualizado webScrapinp

--- webScraping/src/webScraping/ClienteHTTP.java
+++ webScraping/src/webScraping/ClienteHTTP.java
@@ -5,7 +5,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ClienteHTTP {
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,14 +19,27 @@ public class ClienteHTTP {
 		
 		// LA URL QUE QUEREMOS
 		HttpRequest req = HttpRequest.newBuilder()
-				.uri(URI.create("https://www.meteoclimatic.net/?screen_width=1440"))
+				.uri(URI.create("https://www.meteoclimatic.net/perfil/ESARA2200000022002A"))
 				.GET()
 				.build();
 		
-		// RESPUESTA
-		HttpResponse res = cliente.send(req,HttpResponse.BodyHandlers.ofString());
+		// RESPUESTA --> Recibo la respuesta como string
+		HttpResponse<String> res = cliente.send(req,HttpResponse.BodyHandlers.ofString());
+		// Recibo la respuesta como fichero descargado
+		//HttpResponse<Path> resPath = cliente.send(req, HttpResponse.BodyHandlers.ofFile(Paths.get("descargado.html")));
+		// Recibir la respuesta mediante un stream
+		//HttpResponse<InputStream> resInputStream = cliente.send(req, HttpResponse.BodyHandlers.ofInputStream());
+		
+		Document doc = Jsoup.parse(res.body());
+		
+		Elements elementos = doc.select(".titolseccio");
+		
+		for (Element e : elementos) {
+			System.out.println(e.text());
+		}
 		
 		System.out.println(res);
-		System.out.println(res.body());
+		System.out.println("--------------------------------");
+		//System.out.println(res.body());
 	}
 }
